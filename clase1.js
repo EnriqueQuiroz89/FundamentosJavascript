18cbd3a1c743a4e17e6ca8f10c12f2277b7ef4d5
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-27 11:26:49 -0600

    Primer comit

---
+++ clase1.js
@@ -0,0 +1,12 @@
+//JS debilmente tipado
+var edad= 28
+
+edad = '28 años'
+
+//definir varaible   //Se puede o no ocupar ;
+var nombre = 'Sasha', apellido = 'Newton'
+//Si lo voy a escribir para semajar a JAva
+console.log('Hola ' + nombre +" "+apellido) 
+console.log('Edad:  '+ edad)
+
+var peso = 75
