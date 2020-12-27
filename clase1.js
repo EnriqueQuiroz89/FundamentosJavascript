9394111f8f89d762f6da2df063c9c05254af3819
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-27 13:07:32 -0600

    DescripcinonDeFunciones

--- clase1.js
+++ clase1.js
@@ -1,45 +1,20 @@
+//funciones
 
+var nombre = "LeoMesii" , edad =28
 
-//Muestar ultima letra del nombre
-var nombreQuiroz= "Enrique"
+//function es palabalra reservada
 
-var dimNombre = nombreQuiroz.length
+function imprimirEdad(name, age){
 
-var ultimaLetra = nombreQuiroz.substr(dimNombre-1,1)
+console.log(`${name} ${age} años `)
 
+}
 
-//NUMEROS
+imprimirEdad(nombre, edad)
 
-var edad= 27
-//edad = edad + 1
-edad +=1
-var peso = 75
+imprimirEdad('vicky',28)
+imprimirEdad('dario',27)
+imprimirEdad('le',32)
 
-//peso = peso - 2
-peso -=2
-
-var sandwich = 1
-peso = peso +  sandwich
-var jugarAFutbol = 3
-peso = peso - jugarAFutbol
-
-//la precision no estan buena
-var precioVino= 234.3 
-var total =precioVino*3
-
-
-//lo convierte en String
-var totalString = total.toFixed(2)
-
-// regresa a ser Int
-var total2 = parseFloat(totalString);
-
-var pizza = 8
-
-var personas = 2
-
-var cantidadProcionPorPersona = pizza/ personas
-
-
-
-
+//JS no valida las varibles
+imprimirEdad(53, 'invertido')
