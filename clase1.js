7073844c77c32b70621519dc5c7b0c09cdbf886c
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-27 12:04:55 -0600

    Ultima letradel nombre

--- clase1.js
+++ clase1.js
@@ -1,24 +1,11 @@
 
-//definir varaible   //Se puede o no ocupar ;
-var nombre = 'Federico', apellido = 'Newton'
+//Muestar ultima letra del nombre
+var nombreQuiroz= "Enrique"
 
-var nombreEnMayuscula= nombre.toUpperCase()
+var dimNombre = nombreQuiroz.length
 
-var apellidoEnMinusculas = apellido.toLowerCase()
+var ultimaLetra = nombreQuiroz.substr(dimNombre-1,1)
 
-//Primera leetral del nombre
-var primeraLetra = nombre.charAt(0)
-
-var cantidadLetrasNombre = nombre.length // es atributo no funcion
-//concatenar Strings con espacio vacio
-
-
-// otra forma de concatenar con INTERPOLACION
-//comilla invertida
-var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
-
-//Aceder a un substring
-var holaSub= nombre.substr(2,4)
 
 
 
