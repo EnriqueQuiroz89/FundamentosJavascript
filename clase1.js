5792344725e6589b342d8297a9d5131bc649e7b2
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-27 12:29:47 -0600

    Contenido de numeros

--- clase1.js
+++ clase1.js
@@ -1,4 +1,5 @@
 
+
 //Muestar ultima letra del nombre
 var nombreQuiroz= "Enrique"
 
@@ -6,6 +7,38 @@ var dimNombre = nombreQuiroz.length
 
 var ultimaLetra = nombreQuiroz.substr(dimNombre-1,1)
 
+
+//NUMEROS
+
+var edad= 27
+//edad = edad + 1
+edad +=1
+var peso = 75
+
+//peso = peso - 2
+peso -=2
+
+var sandwich = 1
+peso = peso +  sandwich
+var jugarAFutbol = 3
+peso = peso - jugarAFutbol
+
+//la precision no estan buena
+var precioVino= 234.3 
+var total =precioVino*3
+
+
+//lo convierte en String
+var totalString = total.toFixed(2)
+
+// regresa a ser Int
+var total2 = parseFloat(totalString);
+
+var pizza = 8
+
+var personas = 2
+
+var cantidadProcionPorPersona = pizza/ personas
 
 
 
