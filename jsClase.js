f2d0a482da2defa7f8a44dd852d960e5e48c0640
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-28 12:51:25 -0600

    Simplificando CALLBACK hell pero sigue siendo un HELL

--- jsClase.js
+++ jsClase.js
@@ -12,24 +12,36 @@ const opts = {crossDomain: true} //opciones
 //const onPeopleResponse = function(persona){ console.log(`Hola yo soy ${persona.name}`) }
 //Nos permite hacer un Request
 
+                                /**TENEMOS 2 CALLBACK EN LA MISMA FUNCION */
 function obtenerPersonaje(id, callback ){
     const url= `${API_URL}${PEOPLE_URL.replace(':id',id)}`  
-    $.get(url, opts, function(persona){ console.log(`Hola yo soy ${persona.name}`)} )
-
-    //Si nos envian un callback
-    if(callback){
-        callback()
-    }
-
-
+   
+     //   ¿donde?  ¿como?  ¿si exito ...? ¿Si fallo?
+    $.get(url,     opts,     callback)
+     .fail( ()=>{
+        console.log(`No se pudo obtener el personaje ${id}`)
+                }
+          )  
 }
+
 
 //esto es peticiones en Serie y no en ASCINCRONISMO
 
-  //CALL BACK HELL
+  //CALL BACK HELLL
 
-obtenerPersonaje(1, function(){
-    obtenerPersonaje(2, function(){
-        obtenerPersonaje(3, null)
-    })
-})
+obtenerPersonaje(1, function(persona){  //HACE DOS COSAS
+/**1*/  console.log(`Soy ${persona.name}`)
+/**2*/obtenerPersonaje(2, function(persona){ //hace 2 cosas
+   /**1*/ console.log(`Soy ${persona.name}`)
+   /**2*/ obtenerPersonaje(3, function(persona){ //hace 2 cosas
+       /**1*/console.log(`Soy ${persona.name}`)
+       /**2*/obtenerPersonaje(4,function(persona){ //hace 2 cosas
+           /**1*/console.log(`Soy ${persona.name}`)
+           /**2*/obtenerPersonaje(5, function(persona){ //hace 2 cosas
+                /**1*/console.log(`Soy ${persona.name}`)
+                /**2*/obtenerPersonaje(6, null)
+                                              })
+                                         })
+                                      })
+                                  })
+                        })
