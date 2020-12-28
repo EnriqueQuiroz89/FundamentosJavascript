c67e33b7c97f513edb708be07f431f9b51c10ddc
HenryDev <enrique.quiroz.89@gmail.com>
2020-12-28 12:12:06 -0600

    Se demuestra mustra una ejecucion en serie con callback HELL

--- jsClase.js
+++ jsClase.js
@@ -9,18 +9,27 @@ const PEOPLE_URL = 'people/:id'
 const lukeUrl = `${API_URL}${PEOPLE_URL.replace(':id',1)}`  //interpolacion
 const opts = {crossDomain: true} //opciones
 
-const onPeopleResponse = function(persona){ console.log(`Hola yo soy ${persona.name}`) }
+//const onPeopleResponse = function(persona){ console.log(`Hola yo soy ${persona.name}`) }
 //Nos permite hacer un Request
 
-function obtenerPersonaje(id){
+function obtenerPersonaje(id, callback ){
     const url= `${API_URL}${PEOPLE_URL.replace(':id',id)}`  
-    $.get(url, opts, onPeopleResponse )
+    $.get(url, opts, function(persona){ console.log(`Hola yo soy ${persona.name}`)} )
+
+    //Si nos envian un callback
+    if(callback){
+        callback()
+    }
+
+
 }
 
-//demostracion del asincronismo 
-//No sabemos en que orden nos va a llegar cada una
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
-obtenerPersonaje(4)
-obtenerPersonaje(5)
+//esto es peticiones en Serie y no en ASCINCRONISMO
+
+  //CALL BACK HELL
+
+obtenerPersonaje(1, function(){
+    obtenerPersonaje(2, function(){
+        obtenerPersonaje(3, null)
+    })
+})
